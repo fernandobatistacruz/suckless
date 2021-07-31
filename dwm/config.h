ac91281ee57efcd58f11fb94ab61930806c0d0f3
fernandobatistacruz <fernandobatistacruz@gmail.com>
2021-07-31 09:59:05 -0300

    Aumento da fonte barra do DWM

--- dwm/config.h
+++ dwm/config.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 0;       /* gap pixel between windows */
+static const unsigned int gappx     = 0;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monospace:size=13", "FontAwesome:pixelsize=17:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Monospace:size=14", "FontAwesome:pixelsize=17:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
