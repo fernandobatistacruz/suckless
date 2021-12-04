fe429d7a225ea073784a23370ea69bf8b5eeea66
fernandobatistacruz <fernandobatistacruz@gmail.com>
2021-12-04 08:55:55 -0300

    Alterado font size dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Monospace:size=13", "FontAwesome:pixelsize=19:antialias=true:autohint=true" };
+static const char *fonts[]          = { "Monospace:size=13", "FontAwesome:pixelsize=20:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Monospace:size=13";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
