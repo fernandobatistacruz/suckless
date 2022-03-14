a8027241b814b09869d5db61fd74912258cedda9
fernandobatistacruz <fernandobatistacruz@gmail.com>
2022-03-14 12:48:08 -0300

    Add Surf Browser

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -36,6 +36,7 @@ static const Rule rules[] = {
 	/* class      instance                title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,                   NULL,       0,            1,           -1 },
 	{ NULL,      "system-config-printer", NULL,       0,            1,           -1 },
+    { "Surf",     NULL,                   NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -72,7 +73,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]       = { "st", NULL };
-static const char *firefoxcmd[]    = { "firefox", NULL };
+static const char *firefoxcmd[]    = { "surf", NULL };
 static const char *scrotcmd[]      = { "scrot", "-s", "-f", "-q", "100", NULL };
 static const char *slockcmd[]      = { "slock", NULL };
 static const char *lightdowncmd[]  = { "light", "-U", "1", NULL };
