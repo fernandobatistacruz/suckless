9c1c40c302b2579bed97e56acb8b86059c144f98
fernandobatistacruz <fernandobatistacruz@gmail.com>
2022-05-07 13:44:12 -0300

    Ajuste Ãslsatus

---
+++ data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+d=`date '+%-d/%-m/%Y %H:%M'`
+echo "$d"
--- slstatus/config.h
+++ slstatus/config.h
@@ -65,11 +65,11 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
     /* function     format     argument */
-    { run_command,  " %s ",      "~/suckless/git-status.sh" },
-     { run_command,  "%s ",      "~/suckless/network.sh" },
-    { run_command,  "%s ",      "~/suckless/cpu.sh" },   
-    { run_command,  "%s ",      "~/suckless/bateria.sh" },
-    { run_command,  "%s ",      "~/suckless/volume.sh" },
-    { datetime,     "%s ",      " %d/%m/%Y %R" },
+    { run_command,  " %s",     "~/suckless/git-status.sh" },
+    { run_command,  " %s",     "~/suckless/network.sh" },
+    { run_command,  " %s",     "~/suckless/cpu.sh" },   
+    { run_command,  " %s",     "~/suckless/bateria.sh" },
+    { run_command,  " %s",     "~/suckless/volume.sh" },
+    { run_command,  " %s ",    "~/suckless/data.sh" },
 };
 
