ec4945529561c636dd7252d712b3eb728581e48b
fernandobatistacruz <fernandobatistacruz@gmail.com>
2022-06-11 11:59:27 -0300

    Update sucklessorg master

--- scroll/config.def.h
+++ scroll/config.def.h
@@ -12,5 +12,5 @@ struct rule rules[] = {
 	{"\033[6;2~",   SCROLL_DOWN, -1},	/* [Shift] + [PageDown] */
 	/* mouse binding shadows ^E and ^Y, so it's disabled by default */
 	{"\031",        SCROLL_UP,    1},	/* mouse wheel up */
-    {"\005",        SCROLL_DOWN,  1}, /* mouse wheel Down */
+	{"\005",        SCROLL_DOWN,  1},	/* mouse wheel Down */
 };
