07ba9e5eb5ef8e34bcc578c1f8d3bc2f09e2a3f7
fernandobatistacruz <fernandobatistacruz@gmail.com>
2022-09-11 15:51:52 -0300

    Add regra Feh

--- dwm/config.h
+++ dwm/config.h
@@ -37,6 +37,7 @@ static const Rule rules[] = {
 	/* class      instance                title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,                   NULL,       0,            1,           -1 },
 	{ "vlc",      NULL,                   NULL,       0,            1,           -1 },
+	{ "feh",      NULL,                   NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
